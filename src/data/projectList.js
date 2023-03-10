9e4562c01e54025e8f090d951aa12265b87fccee
Ahmad Alhaji Mustafa <ahmadmg@gmail.com>
2023-03-10 12:48:14 +0100

    @

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -1,9 +1,8 @@
 import Proj1 from "../assets/Stavanger.jpg";
 import Proj2 from "../assets/SolarSystem.jpeg";
-import Proj3 from "../assets/proj3.jpeg";
-import Proj4 from "../assets/proj4.jpeg";
-import Proj5 from "../assets/proj5.jpeg";
-import Proj6 from "../assets/proj6.jpeg";
+import Drumkit from "../assets/DrumKit.jpg";
+import Portfolio from "../assets/Portfolio.jpeg";
+
 
 export const ProjectList = [
   {
@@ -12,32 +11,24 @@ export const ProjectList = [
     skills: ["SiHtml5","SiCss3"],
     code:"https://github.com/ahmadmg/Parallax-Website-Stavanger",
     preview:"https://ahmadmg.github.io/Parallax-Website-Stavanger/",
+  }, {
+    name: "Portfolio",
+    image: Portfolio,
+    skills: ["SiHtml5","SiCss3","SiJavascript","SiReact"], 
+    code:"https://github.com/ahmadmg/portfolio",
+    preview:"https://ahmadmg.github.io/portfolio/",
   },
-  {
+   {
+    name: "Drumkit",
+    image: Drumkit,
+    skills: ["SiHtml5","SiCss3","SiJavascript"],
+	 code:"https://github.com/ahmadmg/DrumKit",
+    preview:"https://ahmadmg.github.io/DrumKit/",
+  },{
     name: "Solar System",
     image: Proj2,
     skills: ["SiHtml5","SiCss3","SiJavascript"], 
     code:"https://github.com/ahmadmg/GrupperA3SolSystem",
     preview:"https://ahmadmg.github.io/GrupperA3SolSystem/",
   },
-  {
-    name: "Project33333",
-    image: Proj3,
-    skills: ["SiHtml5","SiCss3","SiJavascript","SiReact"],
-  },
-  {
-    name: "Project33",
-    image: Proj4,
-    skills: ["SiHtml5","SiCss3","SiJavascript","SiReact"],
-  },
-  {
-    name: "Project6666",
-    image: Proj5,
-    skills: ["SiHtml5","SiCss3","SiJavascript","SiReact"],
-  },
-  {
-    name: "24243",
-    image: Proj6,
-    skills: ["SiHtml5","SiCss3","SiJavascript","SiReact"],
-  },
-];
+]
