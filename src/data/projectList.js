bfd51ac2d76fbb4440bcf03db335d5dfa0d88c8a
Ahmad Alhaji Mustafa <ahmadmg@gmail.com>
2023-03-14 13:08:15 +0100

    @2222222222

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -6,29 +6,28 @@ import Portfolio from "../assets/Portfolio.png";
 
 export const ProjectList = [
   {
-    name: "Stavanger",
-    image: Proj1,
-    skills: ["SiHtml5","SiCss3"],
+    name:"Stavanger",
+    image:Proj1,
+    skills:["SiHtml5","SiCss3"],
     code:"https://github.com/ahmadmg/Parallax-Website-Stavanger",
-    preview:"https://ahmadmg.github.io/Parallax-Website-Stavanger/",
-  }, {
-    name: "Portfolio",
-    image: Portfolio,
-    skills: ["SiHtml5","SiCss3","SiJavascript","SiReact"], 
+    preview:"https://ahmadmg.github.io/Parallax-Website-Stavanger/"
+  },{
+    name:"Portfolio",
+    image:Portfolio,
+    skills:["SiHtml5","SiCss3","SiJavascript","SiReact"], 
     code:"https://github.com/ahmadmg/portfolio",
-    preview:"https://ahmadmg.github.io/portfolio/",
-  },
-   {
-    name: "Drumkit",
+    preview:"https://ahmadmg.github.io/portfolio/"
+  },{
+    name:"Drumkit",
     image: Drumkit,
     skills: ["SiHtml5","SiCss3","SiJavascript"],
 	 code:"https://github.com/ahmadmg/DrumKit",
-    preview:"https://ahmadmg.github.io/DrumKit/",
+    preview:"https://ahmadmg.github.io/DrumKit/"
   },{
-    name: "Solar System",
-    image: Proj2,
-    skills: ["SiHtml5","SiCss3","SiJavascript"], 
+    name:"Solar System",
+    image:Proj2,
+    skills:["SiHtml5","SiCss3","SiJavascript"], 
     code:"https://github.com/ahmadmg/GrupperA3SolSystem",
-    preview:"https://ahmadmg.github.io/GrupperA3SolSystem/",
+    preview:"https://ahmadmg.github.io/GrupperA3SolSystem/"
   },
 ]
