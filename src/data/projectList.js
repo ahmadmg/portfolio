a18abd47a8b8f748131d7a2d25ebd7339f0ed885
Ahmad Alhaji Mustafa <ahmadmg@gmail.com>
2023-05-09 10:16:32 +0200

    add projects

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -4,12 +4,25 @@ import Drumkit from "../assets/DrumKit.jpg";
 import Portfolio from "../assets/Portfolio.jpg";
 import dog from "../assets/dog.jpg"
 import newtab from "../assets/newtab.jpg"
+import picstore from "../assets/picstore.jpg"
+import typingspeed from "../assets/typingspeed.jpg"
 
-
-export const ProjectList = [{
-    name:"NewTab xtension",
+export const ProjectList = [
+  { name:"Pic Store",
+image:picstore,
+skills:["SiReact","SiCss3","SiHtml5","TbApi"],
+code:"https://github.com/ahmadmg/pic-store",
+preview:"https://picstore2023.netlify.app/"
+}, 
+{ name:"typingspeedometer",
+image:typingspeed,
+skills:["SiReact","SiCss3","SiHtml5","TbApi"],
+code:"https://github.com/ahmadmg/Typing-Speedometer-App",
+preview:"https://typingspeedometer.netlify.app/"
+},
+   { name:"NewTab xtension",
     image:newtab,
-    skills:["SiHtml5","SiCss3","SiJavascript","TbApi"],
+    skills:["SiJavascript","SiHtml5","SiCss3","TbApi"],
     code:"https://github.com/ahmadmg/chrome-extension",
     preview:"https://chrome.google.com/webstore/detail/new-tab/gbogghhnfnkdaepieedpdllamamlcpkd"
   },
@@ -22,25 +35,25 @@ export const ProjectList = [{
   },{
     name:"Portfolio",
     image:Portfolio,
-    skills:["SiHtml5","SiCss3","SiReact"], 
+    skills:["SiReact","SiHtml5","SiCss3"], 
     code:"https://github.com/ahmadmg/portfolio",
     preview:"https://ahmadmg.github.io/portfolio/"
   },{
     name:"Random Dog Img API",
     image:dog,
-    skills:["SiHtml5","SiCss3","SiReact","TbApi"], 
+    skills:["SiReact","SiHtml5","SiCss3","TbApi"], 
     code:"https://github.com/ahmadmg/randomdogimgapi",
     preview:"https://ahmadmg.github.io/randomdogimgapi/"
   },{
     name:"Drumkit",
     image: Drumkit,
-    skills: ["SiHtml5","SiCss3","SiJavascript"],
+    skills: ["SiJavascript","SiHtml5","SiCss3"],
 	 code:"https://github.com/ahmadmg/DrumKit",
     preview:"https://ahmadmg.github.io/DrumKit/"
   },{
     name:"Solar System",
     image:Proj2,
-    skills:["SiHtml5","SiCss3","SiJavascript"], 
+    skills:["SiJavascript","SiHtml5","SiCss3"], 
     code:"https://github.com/ahmadmg/GrupperA3SolSystem",
     preview:"https://ahmadmg.github.io/GrupperA3SolSystem/"
   },
