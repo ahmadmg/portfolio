e2b22ac493a3fbd1e8803d5b5df22047fbdb6447
Ahmad Alhaji Mustafa <ahmadmg@gmail.com>
2024-03-25 10:43:50 +0100

    Github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:'/portfolio/', not needed to delpoy to Hostinger
+   base:'https://ahmadmg.github.io/portfolio/',// not needed to delpoy to Hostinger
   plugins: [react()],
  
 })
